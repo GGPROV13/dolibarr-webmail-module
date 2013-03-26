906f6adee4fc63d898e1d4d439c83dda167e690f
GUERRIER Kevin <guerrier.k@gmail.com>
2013-03-26 09:51:59 +0100

    Ajout du drop table pour la mise à jour de la table llx_usermailboxconfig

--- sql/llx_usermailboxconfig.sql
+++ sql/llx_usermailboxconfig.sql
@@ -1,10 +1,12 @@
+DROP TABLE IF EXISTS llx_usermailboxconfig;
+
 CREATE TABLE llx_usermailboxconfig
 (
   rowid int(11) NOT NULL AUTO_INCREMENT,
   mailbox_imap_login varchar(255) NOT NULL,
   mailbox_imap_password text NOT NULL,
   mailbox_imap_host varchar(255) NOT NULL,
-  mailbox_imap_port varchar(8) NOT NULL,
+  mailbox_imap_port varchar(8) NOT NULL DEFAULT '993',
   mailbox_imap_ssl boolean NOT NULL DEFAULT 1,
   fk_user int(11) NOT NULL,
   PRIMARY KEY (`rowid`)
